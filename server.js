95774eb70c21297446f86101d62f0c0dd073902e
dipeeshdivakaran <dipeeshdivakaran4@gmail.com>
2016-10-03 10:02:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,65 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone ={
+ title: 'dipeesh tricks',
+ heading: 'arictle one',
+ date: 'sep.5.2016',
+ content:`
+          <p>
+            ekjhdkw.ebqubkfmhbsdcmkvwufgwmmbskjbclglwglgmnkxgfgnbkcjlgfg
+            ekjhdkw.ebqubkfmhbsdcmkvwufgwmmbskjbclglwglgmnkxgfgnbkcjlgfg
+          </P>
+          <p>
+            ekjhdkw.ebqubkfmhbsdcmkvwufgwmmbskjbclglwglgmnkxgfgnbkcjlgfg
+            ekjhdkw.ebqubkfmhbsdcmkvwufgwmmbskjbclglwglgmnkxgfgnbkcjlgfg
+         </P>`
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.data;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+    <head>
+        <title>${content}</title>
+         <link href="/ui/style.css" rel="stylesheet" />
+        <style>
+            
+     </style>
+    </head>
+    <body>
+    <div class="container">
+        <div>
+            <a href="/">home</a>
+        </div>
+     <hr/>
+     <h3>
+       ${hedaing}
+     </h3>
+     <div>
+        ${date}
+     </div>
+     <div>
+        ${content}
+     </div>
+    </div>
+    </body>
+    </html>
+`;
+ return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',  function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTenplate(articleone));
 });
 
 app.get('/article-two',  function (req, res) {
@@ -18,7 +71,7 @@ app.get('/article-two',  function (req, res) {
 });
 
 app.get('/article-three',  function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
